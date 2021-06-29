1299c9835962018381d7b3d22218bcf9bda4d98f
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2021-06-29 11:18:53 +0200

    Update create.ps1

--- create.ps1
+++ create.ps1
@@ -3,26 +3,26 @@
 #
 # Version: 1.0.0.0
 #####################################################
+$action = 'CreateAccount'
 $VerbosePreference = "Continue"
 
 # Initialize default value's
 $config = $configuration | ConvertFrom-Json
 $personObj = $person | ConvertFrom-Json
 $success = $false
-$account_guid = New-Guid
 $auditLogs = New-Object Collections.Generic.List[PSCustomObject]
 
 $account = [PSCustomObject]@{
-    ExternalId = $personObj.ExternalId
-    UserName = $personObj.UserName
-    GivenName = $personObj.Name.GivenName
-    FamilyName = $personObj.Name.FamilyName
+    ExternalId          = $personObj.ExternalId
+    UserName            = $personObj.UserName
+    GivenName           = $personObj.Name.GivenName
+    FamilyName          = $personObj.Name.FamilyName
     FamilyNameFormatted = $personObj.DisplayName
-    FamilyNamePrefix = ''
-    IsUserActive = $true
-    EmailAddress = $personObj.Contact.Business.Email
-    EmailAddressType = 'Work'
-    IsEmailPrimary = $true
+    FamilyNamePrefix    = ''
+    IsUserActive        = $true
+    EmailAddress        = $personObj.Contact.Business.Email
+    EmailAddressType    = 'Work'
+    IsEmailPrimary      = $true
 }
 
 #Region Helper Functions
@@ -55,138 +55,57 @@ function Get-GenericScimOAuthToken {
         }
 
         $body = @{
-            client_id = $ClientID
+            client_id     = $ClientID
             client_secret = $ClientSecret
-            grant_type = "client_credentials"
+            grant_type    = "client_credentials"
         }
 
         $splatRestMethodParameters = @{
-            Uri = $TokenUri
-            Method = 'POST'
+            Uri     = $TokenUri
+            Method  = 'POST'
             Headers = $headers
-            Body = $body
+            Body    = $body
         }
         Invoke-RestMethod @splatRestMethodParameters
         Write-Verbose 'Finished retrieving accessToken'
-    } catch {
+    }
+    catch {
         $PSCmdlet.ThrowTerminatingError($PSItem)
     }
 }
 
-function Invoke-GenericScimRestMethod {
-    <#
-    .SYNOPSIS
-    Post data to a SCIM API <http://www.simplecloud.info/>
-
-    .PARAMETER Uri
-    The Uri to the SCIM API. <http://some-api/v1/scim>
-
-    .PARAMETER Endpoint
-    The path to the specific endpoint being queried. The endpoints follow the standards of the SCIM implementation
-
-    .PARAMETER Headers
-    The headers containing the AccessToken
-
-    .PARAMETER Body
-    The JSON body
-
-    .PARAMETER Method
-    The HTTP method. For example: POST, PATCH or PUT
-
-    .PARAMETER IsConnectionTls12
-    Adds TLS1.2 to the outgoing connection
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [Uri]
-        $Uri,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string]
-        $Endpoint,
-
-        [Parameter(Mandatory = $true)]
-        [System.Collections.IDictionary]
-        $Headers,
-
-        [Parameter(Mandatory = $true)]
-        [object]
-        $Body,
-
-        [Parameter(Mandatory = $true)]
-        [Microsoft.PowerShell.Commands.WebRequestMethod]
-        $Method,
-
-        [Parameter(Mandatory = $false)]
-        [bool]
-        $IsConnectionTls12
-    )
-
-    process {
-        try {
-            Write-Verbose "Invoking command '$($MyInvocation.MyCommand)' to endpoint '$Endpoint'"
-            Write-Verbose "Setting 'Invoke-RestMethod' parameters: '$($PSBoundParameters.Keys)'"
-            $splatRestMethodParameters = @{
-                Uri = "$Uri/$($Endpoint)"
-                Method = $Method
-                ContentType = 'application/json'
-                Headers = $Headers
-            }
-
-            if ($IsConnectionTls12){
-                Write-Verbose 'Switching to TLS 1.2'
-                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
-            }
-
-            if ($body){
-                Write-Verbose 'adding body to request'
-                $splatRestMethodParameters['Body'] = $Body
-            }
-            Invoke-RestMethod @splatRestMethodParameters
-        } catch {
-            $PSCmdlet.ThrowTerminatingError($PSItem)
-        }
-    }
-}
-
 function Resolve-HTTPError {
-    <#
-    .SYNOPSIS
-    Resolves an HTTP error for both Windows PowerShell 5.1 and PowerShell 7.0.3 Core
-    #>
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory,
+            ValueFromPipeline
+        )]
         [object]$ErrorObject
     )
-
-    $HttpErrorObj = @{
-        FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
-        InvocationInfo = $ErrorObject.InvocationInfo.MyCommand
-        TargetObject  = $ErrorObject.TargetObject.RequestUri
+    process {
+        $HttpErrorObj = @{
+            FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
+            MyCommand             = $ErrorObject.InvocationInfo.MyCommand
+            RequestUri            = $ErrorObject.TargetObject.RequestUri
+        }
+        if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
+            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+        } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
+            $stream = $ErrorObject.Exception.Response.GetResponseStream()
+            $stream.Position = 0
+            $streamReader = New-Object System.IO.StreamReader $Stream
+            $errorResponse = $StreamReader.ReadToEnd()
+            $HttpErrorObj['ErrorMessage'] = $errorResponse
+        }
+        Write-Output "'$($HttpErrorObj.ErrorMessage)', TargetObject: '$($HttpErrorObj.RequestUri), InvocationCommand: '$($HttpErrorObj.MyCommand)"
     }
-
-    if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException'){
-        $HttpErrorObj['ErrorMessage'] = (($ErrorObject.ErrorDetails.Message) | ConvertFrom-Json).detail
-    } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException'){
-        $stream = $ErrorObject.Exception.Response.GetResponseStream()
-        $stream.Position = 0
-        $streamReader = New-Object System.IO.StreamReader $Stream
-        $errorResponse = $StreamReader.ReadToEnd()
-        $HttpErrorObj['ErrorMessage'] = ($errorResponse | ConvertFrom-Json).detail
-    }
-
-    Write-Output "'$($HttpErrorObj.ErrorMessage)', TargetObject: '$($HttpErrorObj.TargetObject), InvocationCommand: '$($HttpErrorObj.InvocationInfo)"
 }
 #EndRegion
 
-if(-not($dryRun -eq $true)){
+if (-not($dryRun -eq $true)) {
     try {
-        Write-Verbose "Creating user '$($account.UserName)'"
-        Write-Verbose "Retrieving accessToken"
+        Write-Verbose "Creating account for '$($personObj.DisplayName)'"
+        Write-Verbose 'Retrieving accessToken'
         $accessToken = Get-GenericScimOAuthToken -ClientID $($config.ClientID) -ClientSecret $($config.ClientSecret)
 
         [System.Collections.Generic.List[object]]$roles = @()
@@ -194,29 +113,29 @@ if(-not($dryRun -eq $true)){
         $emailList.Add(
             [PSCustomObject]@{
                 primary = $account.IsEmailPrimary
-                type = $account.EmailAddressType
+                type    = $account.EmailAddressType
                 display = $account.EmailAddress
-                value = $account.EmailAddress
+                value   = $account.EmailAddress
             }
         )
 
         $body = [ordered]@{
-            schemas = @(
+            schemas    = @(
                 "urn:ietf:params:scim:schemas:core:2.0:User",
                 "urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"
             )
             externalId = $account.ExternalID
-            userName = $account.UserName
-            active = $account.IsUserActive
-            emails = $emailList
-            meta = @{
+            userName   = $account.UserName
+            active     = $account.IsUserActive
+            emails     = $emailList
+            meta       = @{
                 resourceType = "User"
             }
             name = [ordered]@{
-                formatted = $account.NameFormatted
-                familyName = $account.FamilyName
+                formatted        = $account.NameFormatted
+                familyName       = $account.FamilyName
                 familyNamePrefix = $account.FamilyNamePrefix
-                givenName = $account.GivenName
+                givenName        = $account.GivenName
             }
             roles = $roles
         } | ConvertTo-Json
@@ -224,33 +143,35 @@ if(-not($dryRun -eq $true)){
         Write-Verbose 'Adding Authorization headers'
         $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
         $headers.Add("Authorization", "Bearer $accessToken")
-
         $splatParams = @{
-            Uri = $($config.BaseUrl)
-            Endpoint = 'Users'
-            Headers = $headers
-            Body = $body
-            Method = 'Post'
+            Uri               = "$($config.BaseUrl)/scim/v2/Users"
+            Headers           = $headers
+            Body              = $body
+            Method            = 'Post'
             IsConnectionTls12 = $($config.IsConnectionTls12)
         }
-        $results = Invoke-GenericScimRestMethod @splatParams
-        Write-Verbose "Finished creating user with id: '$($results.id)'"
-        $success = $true
-        $auditLogs.Add([PSCustomObject]@{
-            Message = "Account for '$($personObj.DisplayName)' successfully created with id: '$($results.id)'"
-            IsError = $False
-        })
+
+        $results = Invoke-RestMethod @splatParams
+        if ($results.id){
+            $logMessage = "Account for '$($personObj.DisplayName)' successfully created with id: '$($results.id)'"
+            Write-Verbose $logMessage
+            $success = $true
+            $auditLogs.Add([PSCustomObject]@{
+                Action  = $action
+                Message = $logMessage
+                IsError = $False
+            })
+        }
     } catch {
         $ex = $PSItem
-        if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')){
+        if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
             $errorMessage = Resolve-HTTPError -Error $ex
             $auditMessage = "Account for '$($personObj.DisplayName)' not created. Error: $errorMessage"
-        }
-        else {
+        } else {
             $auditMessage = "Account for '$($personObj.DisplayName)' not created. Error: $($ex.Exception.Message)"
         }
         $auditLogs.Add([PSCustomObject]@{
-            Action = "CreateAccount"
+            Action  = $action
             Message = $auditMessage
             IsError = $true
         })
@@ -259,10 +180,10 @@ if(-not($dryRun -eq $true)){
 }
 
 $result = [PSCustomObject]@{
-    Success = $success
-    Account = $account
-    AccountReference = $account_guid
-    AuditLogs = $auditLogs
+    Success          = $success
+    Account          = $account
+    AccountReference = $($results.id)
+    AuditLogs        = $auditLogs
 }
 
 Write-Output $result | ConvertTo-Json -Depth 10
